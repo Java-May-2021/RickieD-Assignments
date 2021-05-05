d0c0027cb1907ebc4e4d952c11437b1e9d32a86f
r_dixon_codes <rhdixon79@gmail.com>
2021-05-04 23:57:36 -0700

    initial commit

---
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -0,0 +1,23 @@
+public class StringManipulator{
+    public String trimAndConcat(String string, String otherString) {
+        return string.trim().concat(otherString.trim());
+    }
+    public Integer getIndexOrNull(String input, char c){
+        if(input.indexOf(c) == -1) {
+            return null;
+        }
+        return input.indexOf(c);
+    }
+    public Integer getIndexOrNull(String string, String otherString) {
+        if(string.indexOf(otherString) == -1){
+            return null;
+        }
+        return string.indexOf(otherString);
+
+    }
+    public String concatSubString(String string, int number, int number2, String otherString) {
+        String substr = string.substring(number, number2);
+        return substr.concat(otherString);
+
+    }
+}
