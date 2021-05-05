4692042465618bff35ddf9874c3d178819ab9293
r_dixon_codes <rhdixon79@gmail.com>
2021-05-04 22:15:19 -0700

    initial commit

--- 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -1,7 +1,6 @@
-import java.util.*;
-import java.lang.Math;
 public class Pythagorean {
-    public double calculateHypotenuse(int legA, int legB) {
+    public static double calculateHypotenuse(int legA, int legB) {
+        return Math.sqrt((legA * legA) + (legB * legB));
         // the hypotenuse is the side across from the right angle. 
         // calculate the value of c given legA and legB
     }
