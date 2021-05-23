f86a0dabbcbc4342eb351a2308f6c724192f4e49
r_dixon_codes <rhdixon79@gmail.com>
2021-05-23 09:06:46 -0700

    added challenge goodies

--- 03-java-spring/01-spring-fundamentals/08-ninja-gold/ninja_gold/src/main/java/com/rickied/ninja_gold/controller/NinjaGoldController.java
+++ 03-java-spring/01-spring-fundamentals/08-ninja-gold/ninja_gold/src/main/java/com/rickied/ninja_gold/controller/NinjaGoldController.java
@@ -67,12 +67,17 @@ public class NinjaGoldController {
 				activity.add(String.format("You entere a %s and earned %s gold (" + dtf.format(now) + ")", location, goldEarned));
 			}
 		}
+		//Send the ninja to a debtors prison on a second rendered page if the ninja falls too far into debt 
+		if(gold < -100) {
+			activity.add(String.format("You entered a %s and lost %s gold. Over limit. Jail time for you. (" + dtf.format(now) + ")", location, goldEarned));
+			return "jail.jsp";
+		}
 		int totalGold = gold += goldEarned;
 		session.setAttribute("activity", activity);
 		session.setAttribute("gold", totalGold);
 		return "redirect:/";
-	}
-	//Bonus Challenge: Create a reset button to set the gold earned back to zero
+		}
+		//Bonus Challenge: Create a reset button to set the gold earned back to zero
 		@RequestMapping("/reset")
 		public String clearCount(HttpSession session) {
 			session.setAttribute("gold", 0);// Will reset gold to 0
