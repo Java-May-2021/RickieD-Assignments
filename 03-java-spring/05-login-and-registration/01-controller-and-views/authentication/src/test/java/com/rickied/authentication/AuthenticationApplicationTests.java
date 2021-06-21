c7ecd9cc7f5b37537fc81fe67970dbaf76e83935
r_dixon_codes <rhdixon79@gmail.com>
2021-06-20 22:10:40 -0700

    controller and views assignment

---
+++ 03-java-spring/05-login-and-registration/01-controller-and-views/authentication/src/test/java/com/rickied/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickied.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
